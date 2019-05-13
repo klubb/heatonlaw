b3f0298cdf158740bda30783040d41871eb6f4f5
klubb <unchartedanthem@gmail.com>
2019-05-13 15:02:42 -0600

    added hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,86 +1,119 @@
-import React, {Component} from 'react';
-import './header.css'
-import logo from './heaton-law.png'
+import React, { Component } from "react"
+import "./header.css"
+import logo from "./heaton-law.png"
 import { Link } from "gatsby"
-import { FaPhone, FaClock } from "react-icons/fa";
+import { FaPhone, FaClock, FaBars } from "react-icons/fa"
 
 
 
-const activeStyles = {
-        color: 'red'
-}
+export default class Header extends Component {
 
- export default class Header extends Component {
-    render () {
-        return (
-            <div>
-                <div className='blackheader'>
-                {/* <h1>heatonlawgroup@gmail.com</h1> */}
+        openMenu = () => {
+                document.getElementById('menu').style.height = '33%';
                 
-                
-                
+        }
 
-                
-              
-                
-                
+        closeMenu = () => {
+                document.getElementById('menu').style.height = '0';
+        }
+  render() {
+    return (
+      <div>
+        <div className="blackheader">
+          
 
-                <h1>  Our office hours are 6AM - 5PM. Mon-Fri.</h1>
-                <div className='divspacer'></div>
-                <div className="spacer"></div>
-                <div className='phone'>
-                <FaPhone /> <h1> (801) -709-4117</h1>
+          <h1 className='office'> Our office hours are 6AM - 5PM. Mon-Fri.</h1>
+          <div className="divspacer" />
+          <div className="spacer" />
+          <div className="phone">
+            <FaPhone /> <h1> (801) -709-4117</h1>
+          </div>
+        </div>
+        <div className="navbarContainer">
+          <div>
+            <Link to="/">
+              <img className="logo" src={logo} alt="" />
+            </Link>
+          </div>
+                <nav className='navbar'>
+
+                <span  className='hamburger'>
+                    <FaBars onClick={this.openMenu}/>    
+                </span>
+                
+                </nav>
+
+                <div id='menu' className="nav-dropdown">
+                <div className='nav-items'>
+
+                        <a href="#" className='btn-close' onClick={this.closeMenu}>&times;</a>
+                        <Link to='/'>Home</Link>
+                        <hr className='menuspacer'/>
+                        <Link to='/expert-team'>Our Expert Team</Link>
+                        <hr className='menuspacer'/>
+                        <Link to='/practice-areas'>Practice Areas</Link>
+                        <hr className='menuspacer'/>
+                        <Link to='/contact'>Contact</Link>
+                        <hr className='menuspacer'/>
+                        <Link to='/blog'>Blog</Link>
                 </div>
                 
-                
-                
-                
-                
-                
-
                 </div>
-            <div className = 'navbarContainer'>
-            <div>
-                    <Link to="/"><img className = 'logo' src={logo} alt=""/></Link>
-            </div>
-            
-                    <ul className = 'navLinks'>
+
+          <ul className="navLinks">
             <div class="dropdown  ">
-                    <Link activeClassName='active' to= "/"><span className="dropbtn hvr-fade">Home</span></Link>
-                    
+              <Link activeClassName="active" to="/">
+                <span className="dropbtn hvr-fade">Home</span>
+              </Link>
             </div>
-                    <div class="dropdown">
-                    <Link activeClassName='active' to='/expert-team'><span  className="dropbtn hvr-fade">Our Expert Team</span></Link>
-                    
-            </div>
-
-
             <div class="dropdown">
-                    <Link activeClassName='active' to = '/page-2/'><span class="dropbtn hvr-fade">Practice Areas</span></Link>
-                    <div class="dropdown-content">
-                    <Link activeClassName='active' to="/criminal">Criminal</Link>
-                    <Link partiallyActive={true} activeClassName='active' to ="/litigation">Litigation</Link>
-                    <Link activeClassName='active' to="/injury">Personal injury</Link>
-                    <Link activeClassName='active' to="/auto-accident">Auto Accident</Link>
-                    </div>
+              <Link activeClassName="active" to="/expert-team">
+                <span className="dropbtn hvr-fade">Our Expert Team</span>
+              </Link>
             </div>
 
             <div class="dropdown">
-            <Link partiallyActive={true} activeClassName='active' to="/contact">Contact</Link>
-                    
+              <Link activeClassName="active" to="/page-2/">
+                <span class="dropbtn hvr-fade">Practice Areas</span>
+              </Link>
+              <div class="dropdown-content">
+                <Link activeClassName="active" to="/criminal">
+                  Criminal
+                </Link>
+                <Link
+                  partiallyActive={true}
+                  activeClassName="active"
+                  to="/litigation"
+                >
+                  Litigation
+                </Link>
+                <Link activeClassName="active" to="/injury">
+                  Personal injury
+                </Link>
+                <Link activeClassName="active" to="/auto-accident">
+                  Auto Accident
+                </Link>
+              </div>
             </div>
 
             <div class="dropdown">
-            <Link partiallyActive={true} activeClassName='active' to="/blog">Blog</Link>
-                    
+              <Link
+                partiallyActive={true}
+                activeClassName="active"
+                to="/contact"
+              >
+                Contact
+              </Link>
             </div>
-            
-                        
-                        
-                    </ul>
-                
+
+            <div class="dropdown">
+              <Link partiallyActive={true} activeClassName="active" to="/blog">
+                Blog
+              </Link>
             </div>
-            </div>
-        )
-    }
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
